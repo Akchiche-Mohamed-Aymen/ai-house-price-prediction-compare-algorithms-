3a742abcbb6e6242253c7a998df1b74e48fde4a5
Akchiche-Mohamed-Aymen <akchiche.mohamedaymen@gmail.com>
2025-09-01 13:33:00 +0100

    refactor my code to make it more clean and readable

--- linear_regression.py
+++ linear_regression.py
@@ -1,69 +1,12 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler , OneHotEncoder
-from sklearn.metrics import r2_score , mean_squared_error 
+from sharedCodeForAlgos import create_model , evaluate_model
 import pandas as pd
-from utils import target , createMissedColumns
-import matplotlib.pyplot as plt
-from math import sqrt
-import json
-from numpy import log
 """-------------------------------------Import Tools ----------------------------------------------------------"""
-cols = None
-with open("cols.json" , 'r') as f:
-    cols  = json.load(f)["cols"]
-cols.remove(target)
-df = pd.read_csv('clean_train.csv')
-y = df[target]
-X = df[cols]
-num_cols = X.select_dtypes(include=["int32" , "int64" , "float64"]).columns
-cat_cols = X.select_dtypes(include=["object"]).columns
-
-num_pipe =  Pipeline([
-    ("scale" , StandardScaler())
-])
-cat_pipe =  Pipeline([
-    ("impute" , SimpleImputer(strategy= 'most_frequent')),
-    ("encoder" , OneHotEncoder(handle_unknown = 'ignore'))
-])
-
-""""""
-prepocessor = ColumnTransformer([
-    ("num" , num_pipe , num_cols),
-    ("cat" , cat_pipe , cat_cols)
-    ])
-
-model_pipe = Pipeline([
-    ('prepocessor' , prepocessor),
-    ("regressor" , LinearRegression())
-])
-X_train , X_test ,y_train , y_test = train_test_split(X , y ,test_size = 0.3 , shuffle = True , random_state= 42)
-model_pipe.fit(X_train , y_train)
-y_pred = model_pipe.predict(X_train)  
-evaluate_performance  = {}
-r2 , mse = r2_score(y_train , y_pred) , mean_squared_error(log(y_train) , log(y_pred))
-evaluate_performance["min_train"] =[ min(y_train)]
-evaluate_performance["max_train"] = [max(y_train)]
-evaluate_performance["min_test"] = [min(y_test)]
-evaluate_performance["max_test"] = [max(y_test)]
-evaluate_performance["r2_score_train"] = [round(r2 , 3)]
-evaluate_performance["log_rmse_train"] = [round(sqrt(mse) , 3)]
-y_pred = model_pipe.predict(X_test)
-r2 , mse = r2_score(y_test , y_pred) , mean_squared_error(y_test , y_pred)
-evaluate_performance["r2_score_test"] = [round(r2 , 3)]
-evaluate_performance["log_rmse_test"] = [round(sqrt(mse) , 3)]
-
+model_pipe , X_train , X_test ,y_train , y_test  = create_model(LinearRegression())
+y_pred_train  ,y_pred_test =  model_pipe.predict(X_train) , model_pipe.predict(X_test)
+evaluate_performance  =  evaluate_model(y_pred_train , y_train  , y_pred_test , y_test)   
 pd.DataFrame(evaluate_performance).to_csv("evaluation_performance.csv")
 print(pd.DataFrame(evaluate_performance))
 """ save solution  git push origin main"""
-try:
-    df = pd.read_csv("clean_test.csv")
-    df = df[cols]
-    solution = pd.DataFrame({ 'Id' :list(range(len(df))) , target : model_pipe.predict(df) })
-    solution.to_csv("solution_linear_regression.csv")
-except Exception as e :
-    print(e)
+
 #cls ; py linear_regression.py
---
+++ sharedCodeForAlgos.py
@@ -0,0 +1,77 @@
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler , OneHotEncoder
+from sklearn.metrics import r2_score , mean_squared_error 
+import pandas as pd
+from utils import target 
+import json
+from numpy import log
+from math import sqrt
+
+
+def get_columns():
+    try:
+        with open("cols.json" , 'r') as f:
+            cols = json.load(f)["cols"]
+            return cols
+    except :
+        return list()
+def create_pipelines():
+    num_pipe =  Pipeline([
+        ("scale" , StandardScaler())
+    ])
+    cat_pipe =  Pipeline([
+        ("impute" , SimpleImputer(strategy= 'most_frequent')),
+        ("encoder" , OneHotEncoder(handle_unknown = 'ignore'))
+    ])
+    return  num_pipe ,  cat_pipe
+def create_model(algo):
+    cols = get_columns()
+    cols.remove(target)
+    df = pd.read_csv('clean_train.csv')
+    y = df[target]
+    X = df[cols]
+    num_cols = X.select_dtypes(include=["int32" , "int64" , "float64"]).columns
+    cat_cols = X.select_dtypes(include=["object"]).columns
+    num_pipe ,  cat_pipe = create_pipelines()
+    prepocessor = ColumnTransformer([
+        ("num" , num_pipe , num_cols),
+        ("cat" , cat_pipe , cat_cols)
+        ])
+    model_pipe = Pipeline([
+        ('prepocessor' , prepocessor),
+        ("regressor" , algo)
+    ])
+    X_train , X_test ,y_train , y_test = train_test_split(X , y ,test_size = 0.3 , shuffle = True , random_state= 42)
+    model_pipe.fit(X_train , y_train)
+    cls = model_pipe.named_steps["regressor"].__class__.__name__
+    save_solution(model_pipe , cols , f"sol_with_{cls}.csv")
+    return model_pipe , X_train , X_test ,y_train , y_test
+
+def save_solution(model_pipe , cols , fname):
+    try:
+        df = pd.read_csv("clean_test.csv")
+        df = df[cols]
+        solution = pd.DataFrame({ 'Id' :list(range(len(df))) , target : model_pipe.predict(df) })
+        solution.to_csv(fname)
+        with open(".gitignore" , "a")as f:
+            f.write(fname)
+            f.write('\n')
+    except Exception as e :
+        print(e)
+        
+def evaluate_model( y_pred_train ,y_train ,y_pred_test ,  y_test):
+    evaluate_performance  = {}
+    r2 , mse = r2_score(y_train , y_pred_train) , mean_squared_error(log(y_train) , log(y_pred_train))
+    evaluate_performance["min_train"] =[ min(y_train)]
+    evaluate_performance["max_train"] = [max(y_train)]
+    evaluate_performance["min_test"] = [min(y_test)]
+    evaluate_performance["max_test"] = [max(y_test)]
+    evaluate_performance["r2_score_train"] = [round(r2 , 3)]
+    evaluate_performance["log_rmse_train"] = [round(sqrt(mse) , 3)]
+    r2 , mse = r2_score(y_test , y_pred_test) , mean_squared_error(y_test , y_pred_test)
+    evaluate_performance["r2_score_test"] = [round(r2 , 3)]
+    evaluate_performance["log_rmse_test"] = [round(sqrt(mse) , 3)]
+    return evaluate_performance
